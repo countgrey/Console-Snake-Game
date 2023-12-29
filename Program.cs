25be246a0840ce1ed5b1a06345bd9c5baa11845e
admin <admin@REDMIBOOK>
2023-12-29 16:44:33 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿namespace SnakeGame;
+
+class Program
+{
+    private const int width = 100;
+    private const int height = 100;
+
+    private const ConsoleColor borderColor = ConsoleColor.White;
+
+    public static void Main()
+    {
+        Console.SetWindowSize(width, height);
+        Console.SetBufferSize(width+1, height+1);
+        Console.CursorVisible = false;
+
+        new Pixel(10, 10, borderColor).Draw();
+        DrawBorder();
+
+        Console.ReadKey();
+    }
+
+    private static void DrawBorder()
+    {
+        for (int y = 0; y <= height; y++)
+        {
+            for (int x = 0; x <= width; x++)
+            {
+                if (x == width-1 || y == height-1 || x == 0 || y == 1)
+                {
+                    new Pixel(x, y, borderColor).Draw();
+                }
+            }
+            Console.WriteLine();
+        }
+
+    }
+}
---
+++ Structs/Pixel.cs
@@ -0,0 +1,22 @@
+﻿namespace SnakeGame;
+
+public readonly struct Pixel(int x, int y, ConsoleColor color)
+{
+    private readonly char _char = '█';
+
+    public int X { get; } = x;
+    public int Y { get; } = y;
+    public ConsoleColor Color { get; } = color;
+
+    public void Draw()
+    {
+        Console.SetCursorPosition(X, Y);
+        Console.Write(_char);
+    }
+
+    public void Clear()
+    {
+        Console.SetCursorPosition(X, Y);
+        Console.Write(" ");
+    }
+}
